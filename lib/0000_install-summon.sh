f4b667729969337d177995c1f15c1b38f2c58699
John Tuttle <jtuttle@conjur.net>
2018-02-09 18:45:05 -0500

    CONJ-4720 - Buildpack has Summon version pinned (#12)

--- lib/0000_install-summon.sh
+++ lib/0000_install-summon.sh
@@ -15,8 +15,8 @@ _conjur_BIN_DIR="$_conjur_BUILD_DIR/bin"
 _conjur_ESCAPED_BIN_DIR=$(echo "$_conjur_BIN_DIR" | sed -e 's/[\/&]/\\&/g')
 
 mkdir -p ${_conjur_BIN_DIR}
-curl -sSL https://raw.githubusercontent.com/cyberark/summon/master/install.sh | sed -e 's/sudo //' -e "s/\/usr\/local\/bin/$_conjur_ESCAPED_BIN_DIR/" -e "s/-d \"\/etc\/bash_completion.d\"/\! 1/" | bash
-curl -sSL https://raw.githubusercontent.com/cyberark/summon-conjur/master/install.sh | sed -e 's/sudo //' -e "s/\/usr\/local\/lib\/summon/$_conjur_ESCAPED_BIN_DIR/" | bash
+curl -sSL https://raw.githubusercontent.com/cyberark/summon/v0.6.6/install.sh | sed -e 's/sudo //' -e "s/\/usr\/local\/bin/$_conjur_ESCAPED_BIN_DIR/" -e "s/-d \"\/etc\/bash_completion.d\"/\! 1/" | bash
+curl -sSL https://raw.githubusercontent.com/cyberark/summon-conjur/v0.5.0/install.sh | sed -e 's/sudo //' -e "s/\/usr\/local\/lib\/summon/$_conjur_ESCAPED_BIN_DIR/" | bash
 
 unset -f _conjur_ESCAPED_BIN_DIR _conjur_BIN_DIR _conjur_ESCAPED_BIN_DIR
 trap - ERR
