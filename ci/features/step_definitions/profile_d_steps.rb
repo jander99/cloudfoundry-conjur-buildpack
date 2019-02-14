e394cdfa7c904b0dd66c447a79ff03a05ec06255
John Tuttle <jtuttle@conjur.net>
2019-02-14 13:31:22 -0500

    remove buildpackless test as duplicate, add comments

--- ci/features/step_definitions/profile_d_steps.rb
+++ ci/features/step_definitions/profile_d_steps.rb
@@ -1,5 +1,8 @@
 When(/^the retrieve secrets \.profile\.d script is sourced$/) do
   @commands ||= []
+
+  # Set $HOME and $DEPS_DIR to mimic variables that are made available to the
+  # script when run normally via profile.d.
   @commands << <<EOL
 HOME=#{@BUILD_DIR} DEPS_DIR=#{@DEPS_DIR} . #{@DEPS_DIR}/#{@INDEX_DIR}/.profile.d/0001_retrieve-secrets.sh
 EOL
